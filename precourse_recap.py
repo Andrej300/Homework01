ddfba2575f7ca9125c6e5e3c3899d96f15ff0df4
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2020-11-17 00:27:29 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,22 @@
+print("Welcome here, let's start the game!")
+var = input("1. What is 5+5? ")
+
+if var == '10':
+  print("Correct")
+else:
+  print("Incorrect")
+
+var = input("2. What is 3*3? ")
+
+if var == '9':
+  print("Correct")
+else:
+  print ("Incorrect")
+
+var = input("3. What is the capital of Hungary? ")
+
+if var == 'Budapest':
+  print("Well done! Thank you for playing!")
+else:
+  print("Incorrect")
+  print("That's the end, thank you for playing!")
